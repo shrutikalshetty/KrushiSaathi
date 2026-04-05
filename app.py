2ed6739048bca6e03d13c3d6d57c69d3bf21209f
shrutikalshetty <shrutikalshetty04@gmail.com>
2026-04-05 06:16:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -319,3 +319,8 @@ def graph():
 
 if __name__ == "__main__":
     app.run(debug=True)
+#-------------------------------------------
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
